06ce56d2275c3b4d7fcd5fd0c06ece58d88ad0e0
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-07-08 00:01:34 +0200

    Add a basic React client for rendering frontend

---
+++ client/src/components/AppContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AppContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, it's about me!</h1>
+                <p>About me</p>
+            </div>
+        )
+    }
+}
+
+export default AppContainer;
