e1fd0558aa217feb91fa4abc91af60e200f2f9c1
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-07-08 01:12:02 +0200

    Add CORS support

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const skillsRouter = require('./routes/skills.route');
@@ -8,6 +9,7 @@ connectDatabase();
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
