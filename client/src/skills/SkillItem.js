44250e492ae5a07403ff8179829523642cc0f182
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-08-20 23:09:06 +0200

    Drop the edit mode

--- client/src/skills/SkillItem.js
+++ client/src/skills/SkillItem.js
@@ -1,36 +1,26 @@
 import { Component } from "react";
-import { ListItem, ListItemText, TextField } from "@material-ui/core";
+import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 
 class SkillItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
             description: props.data.description,
-            level: props.data.level,
-            editMode: false,
+            level: props.data.level
         };
     }
 
-    toggleEditMode = () => {
-        this.setState(prevState => ({ editMode: !prevState.editMode }));
-    }
-
     render() {
-        let { description, level, editMode } = this.state;
+        let { description, level } = this.state;
 
-        if (editMode) {
-            return (
-                <ListItem>
-                    <TextField onClick={this.toggleEditMode} value={description} onChange={(e) => this.setState({ description: e.target.value })} />
-                </ListItem>
-            );
-        } else {
-            return (
-                <ListItem>
-                    <ListItemText onClick={this.toggleEditMode} primary={description} secondary={"Level: " + level} />
-                </ListItem>
-            );
-        }
+        return (
+            <ListItem>
+                <ListItemIcon>
+                    {/* Todo: add an appropriate icon */}
+                </ListItemIcon>
+                <ListItemText onClick={this.toggleEditMode} primary={description} secondary={"Level: " + level} />
+            </ListItem>
+        );
     }
 }
 
