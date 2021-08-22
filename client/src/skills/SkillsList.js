82eda91161b1b0836dbbae213ab509642a6fb5d4
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-08-22 20:42:22 +0200

    Make the list of skills expandable

--- client/src/skills/SkillsList.js
+++ client/src/skills/SkillsList.js
@@ -1,24 +1,44 @@
 import React, { Component, Fragment } from "react";
-import { List, Typography, Paper, Box } from "@material-ui/core";
+import { List, Typography, Paper, Box, Divider, Button } from "@material-ui/core";
 
 import * as SkillsAPI from "./SkillsAPI";
 import SkillItem from "./SkillItem";
+
+const INITIAL_SHOW_COUNT = 4;
 
 class SkillsList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            skills: []
+            skills: [],
+            showCount: INITIAL_SHOW_COUNT,
+            showAll: false,
         };
+    }
+
+    onShowAllClick() {
+        this.setState(prevState => ({
+            showCount: prevState.skills.length,
+            showAll: true,
+        }));
+    }
+
+    onShowLessClick() {
+        this.setState(prevState => ({
+            showCount: Math.min(INITIAL_SHOW_COUNT, prevState.skills.length),
+            showAll: false,
+        }));
     }
 
     componentDidMount() {
         SkillsAPI.getSkills()
             .then(response => response.json())
-            .then(skills => this.setState({ skills: skills }));
+            .then(skills => this.setState({ skills: skills, showCount: Math.min(INITIAL_SHOW_COUNT, skills.length) }));
     }
 
     render() {
+        let { skills, showCount, showAll } = this.state;
+
         return (
             <Fragment>
                 <Box my={4}>
@@ -29,8 +49,17 @@ class SkillsList extends Component {
 
                 <Paper elevation={0} variant="outlined">
                     <List>
-                        {this.state.skills.map(skill => <SkillItem key={skill.id} data={skill} />)}
+                        {skills.slice(0, showCount).map(skill => <SkillItem key={skill.id} data={skill} />)}
                     </List>
+                    <Divider />
+                    {skills.length > INITIAL_SHOW_COUNT &&
+                        <Box textAlign="center">
+                            {showAll ?
+                                <Button fullWidth color="primary" onClick={() => this.onShowLessClick()}>Show less</Button> :
+                                <Button fullWidth color="primary" onClick={() => this.onShowAllClick()}>Show all ({skills.length})</Button>
+                            }
+                        </Box>
+                    }
                 </Paper>
             </Fragment>
         );
