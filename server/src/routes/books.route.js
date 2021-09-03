ac549808c58e15b13f7816d688e9feeba513e210
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-09-03 23:08:55 +0200

    Add validation and error handling for books

--- server/src/routes/books.route.js
+++ server/src/routes/books.route.js
@@ -5,40 +5,60 @@ const auth = require('../auth/basic.auth');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const books = await BooksService.getAll();
-    res.send(books);
+    try {
+        const books = await BooksService.getAll();
+        res.send(books);
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
 });
 
 router.post('/', auth, async (req, res) => {
-    const newBook = req.body;
-    const bookCreated = await BooksService.create(newBook);
-    res.send(bookCreated.toJSON());
+    try {
+        const newBook = req.body;
+        const bookCreated = await BooksService.create(newBook);
+        res.send(bookCreated.toJSON());
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
 });
 
-router.get('/:bookId', async (res, req, next) => {
-    const book = await BooksService.get(req.params.bookId);
+router.get('/:bookId', async (req, res, next) => {
+    try {
+        const book = await BooksService.get(req.params.bookId);
 
-    if (!book) {
-        return next();
+        if (!book) {
+            return res.sendStatus(404);
+        }
+
+        res.send(book.toJSON());
+    } catch (e) {
+        res.status(400).send(e.message);
     }
-
-    res.send(book.toJSON());
 });
 
 router.put('/:bookId', auth, async (req, res) => {
-    const book = req.body;
-    book.id = req.params.bookId;
+    try {
+        const book = req.body;
+        book.id = req.params.bookId;
 
-    const bookUpdated = await BooksService.update(book);
+        const bookUpdated = await BooksService.update(book);
 
-    res.send(bookUpdated.toJSON());
+        res.send(bookUpdated.toJSON());
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
 });
 
 router.delete('/:bookId', auth, async (req, res) => {
-    const bookId = req.params.bookId;
-    await BooksService.delete(bookId);
+    try {
+        const bookId = req.params.bookId;
+        await BooksService.delete(bookId);
 
-    res.sendStatus(204);
+        res.sendStatus(204);
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
 });
 
 module.exports = router;
--- server/src/services/books.service.js
+++ server/src/services/books.service.js
@@ -2,22 +2,59 @@ const BooksRepository = require('../data/repositories/books.repository');
 
 module.exports = class BooksService {
     static async getAll() {
-        return await BooksRepository.getAll();
+        return await BooksRepository.getAll()
+            .catch(e => {
+                console.log(`The error while getting books: ${e}`);
+                return;
+            });
     }
 
     static async get(bookId) {
-        return await BooksRepository.get(bookId);
+        return await BooksRepository.get(bookId)
+            .catch(e => {
+                console.log(`The error while getting a book ${bookId}: ${e}`);
+                return;
+            });
     }
 
     static async create(book) {
-        return await BooksRepository.add(book);
+        BooksService.validate(book);
+
+        return await BooksRepository.add(book)
+            .catch(e => {
+                console.log(`The error while creating a book ${book}: ${e}`);
+                return;
+            });
     }
 
     static async update(book) {
-        return await BooksRepository.update(book);
+        BooksService.validate(book)
+
+        return await BooksRepository.update(book)
+            .catch(e => {
+                console.log(`The error while updating a book ${book}: ${e}`);
+                return;
+            });
     }
 
     static async delete(bookId) {
-        return await BooksRepository.remove(bookId);
+        return await BooksRepository.remove(bookId).catch(e => {
+            console.log(`The error while deleting a book ${bookId}: ${e}`);
+            return;
+        });;
+    }
+
+    static validate(book) {
+        if (!book.title) {
+            throw new Error("Book title cannot be empty");
+        }
+
+        if (!book.authors) {
+            throw new Error("Book authors cannot be empty");
+        }
+
+        if (!book.readDate) {
+            throw new Error("Book read date cannot be empty");
+        }
     }
 };
