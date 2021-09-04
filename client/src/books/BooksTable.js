1ad23c552f76a445f3c7e395009a6f862e3079bb
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-09-04 23:29:58 +0200

    Add 'Show more' and 'Show less' to the books table

--- client/src/books/BooksTable.js
+++ client/src/books/BooksTable.js
@@ -1,11 +1,15 @@
 import React, { Component, Fragment } from "react";
-import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Box, Typography, Paper } from "@material-ui/core";
+import { Button, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Box, Typography, Paper } from "@material-ui/core";
 import * as BooksAPI from "./BooksAPI";
+
+const INITIAL_SHOW_COUNT = 5;
 
 class BooksTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            showAll: false,
+            showCount: INITIAL_SHOW_COUNT,
             books: []
         };
     }
@@ -16,7 +20,22 @@ class BooksTable extends Component {
             .then(books => this.setState({ books: books }));
     }
 
+    onShowAllClick() {
+        this.setState(prevState => ({
+            showCount: prevState.books.length,
+            showAll: true,
+        }));
+    }
+
+    onShowLessClick() {
+        this.setState({
+            showCount: INITIAL_SHOW_COUNT,
+            showAll: false,
+        });
+    }
+
     render() {
+        let { showAll, showCount, books } = this.state;
         return (
             <Fragment>
                 <Box my={4}>
@@ -36,15 +55,24 @@ class BooksTable extends Component {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {this.state.books.map((book) => (
+                                {books.slice(0, showCount).map((book) => (
                                     <TableRow key={book.id}>
                                         <TableCell>{book.title}</TableCell>
                                         <TableCell align="right">{book.authors}</TableCell>
                                         <TableCell align="right">{new Date(book.readDate).toLocaleDateString()}</TableCell>
                                     </TableRow>
                                 ))}
+
                             </TableBody>
                         </Table>
+                        {books.length > INITIAL_SHOW_COUNT &&
+                            <Box textAlign="center">
+                                {showAll ?
+                                    <Button fullWidth color="primary" onClick={() => this.onShowLessClick()}>Show less</Button> :
+                                    <Button fullWidth color="primary" onClick={() => this.onShowAllClick()}>Show all ({books.length})</Button>
+                                }
+                            </Box>
+                        }
                     </TableContainer>
                 </Paper>
             </Fragment>
