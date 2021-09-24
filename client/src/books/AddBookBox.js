103e60fe5dc97040b0017a8f722156215c59ecd1
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-09-24 23:48:34 +0200

    Add an empty 'add book' dialog

--- client/src/books/AddBookBox.js
+++ client/src/books/AddBookBox.js
@@ -1,9 +1,43 @@
-import { Component } from "react";
+import { Button, Dialog, DialogTitle, DialogContent } from "@material-ui/core";
+import { Component, Fragment } from "react";
 
 class AddBookBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dialogOpen: false,
+        };
+    }
+
+    openDialog() {
+        this.setState({
+            dialogOpen: true,
+        });
+    }
+
+    closeDialog() {
+        this.setState({
+            dialogOpen: false,
+        });
+    }
 
     render() {
-        return "ADD NEW";
+        let { dialogOpen } = this.state;
+        return <Fragment>
+            <Button fullWidth color="primary" onClick={() => this.openDialog()}>Add new</Button>
+            <Dialog
+                open={dialogOpen}
+                onClose={() => this.closeDialog()}
+                fullWidth
+                maxWidth="sm">
+                    <DialogTitle>
+                        Add a new book
+                    </DialogTitle>
+                    <DialogContent>
+                        Here is the context
+                    </DialogContent>
+            </Dialog>
+        </Fragment>;
     }
 };
 
